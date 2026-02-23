a14ab6290cd2e11302384222d7ffbcea9b96d328
Reema-Trainer <reema.trainer25@gmail.com>
2026-02-23 23:14:16 +0200

    Add TimerScreen widget with state management

--- app_states_example.dart
+++ app_states_example.dart
@@ -1,3 +1,9 @@
+class TimerScreen extends StatefulWidget {
+  const TimerScreen({super.key});
+
+  @override
+  State<TimerScreen> createState() => _TimerScreenState();
+}
 class _TimerScreenState extends State<TimerScreen>
     with WidgetsBindingObserver {
 
